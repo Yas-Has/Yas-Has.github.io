725294876af74549346399ccefceeb3ce21b5925
Yas-Has <yashas@gmail.com>
2021-08-13 15:28:10 -0700

    webcam

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function changeHeaderSize(){
 	document.getElementById("mainh1").style.fontSize = "40pt";
 }
+
+let video = document.querySelector("#videoElement");
+
+if(navigator.mediaDevices.getUserMedia){
+	navigator.mediaDevices.getUserMedia({video: true}).then(function(stream)
+		{video.srcObject = stream;})
+	.catch(function(err0r){console.log("smtgs wrong")})
+}

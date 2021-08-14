130dbef4c6516c44a572f2a7e790251faf4c4fd8
Yas-Has <yashas@gmail.com>
2021-08-13 18:24:20 -0700

    webcam works

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function changeHeaderSize(){
 	document.getElementById("mainh1").style.fontSize = "40pt";
 }
 
-let video = document.querySelector("#videoElement");
 
-if(navigator.mediaDevices.getUserMedia){
-	navigator.mediaDevices.getUserMedia({video: true}).then(function(stream)
-		{video.srcObject = stream;})
-	.catch(function(err0r){console.log("smtgs wrong")})
-}
+// let video = document.querySelector("#videoElement");
+
+// if(navigator.mediaDevices.getUserMedia){
+// 	navigator.mediaDevices.getUserMedia({video: true}).then(function(stream)
+// 		{video.srcObject = stream;})
+// 	.catch(function(err0r){console.log("smtgs wrong")})
+// }

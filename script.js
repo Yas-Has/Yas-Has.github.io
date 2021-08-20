121277b30f0626827f2bf45477bbb867a9892ef7
Yas-Has <yashas@gmail.com>
2021-08-19 17:00:33 -0700

    drawing to canvas works

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function changeHeaderSize(){
-	document.getElementById("mainh1").style.fontSize = "40pt";
+
+function main(){
+  var player = document.getElementById('player');
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+
+  var handleSuccess = function(stream) {
+    player.srcObject = stream;
+  };
+
+  navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+      .then(handleSuccess)
+
+  function drawFrame(video) {
+  ctx.drawImage(video, 0, 0);
+
+    setTimeout(function () {
+      drawFrame(video);
+    }, 10);
+  }
+
+  drawFrame(player);
+
 }
-
-
-// let video = document.querySelector("#videoElement");
-
-// if(navigator.mediaDevices.getUserMedia){
-// 	navigator.mediaDevices.getUserMedia({video: true}).then(function(stream)
-// 		{video.srcObject = stream;})
-// 	.catch(function(err0r){console.log("smtgs wrong")})
-// }

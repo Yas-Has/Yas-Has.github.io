76e909d6a13aef5d98d3b66caa6040ac10a3d8a9
Yas-Has <yashas@gmail.com>
2021-08-21 17:50:18 -0700

    fixed old bugs, new ones arrise

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function main(){
   navigator.mediaDevices.getUserMedia({ audio: false, video: true })
       .then(handleSuccess)
 
+  //color class
   class Color{
 
     constructor(r, g, b, a){
@@ -22,20 +23,20 @@ function main(){
       this.g = g;
       this.b = b;
       this.distVals = [];
-      this.color = "";
 
     }
 
-    absoluteDist(other){
-      return Math.abs(this.r - other.r) + Math.abs(this.g - other.g) + Math.abs(this.b - other.b)
-    }
 
-    set color(color){
-      this.color = color;
+  
+
+
+    distance(other){
+      return Math.pow(Math.pow(this.r - other.r, 2) + Math.pow(this.g - other.g, 2) + Math.pow(this.b - other.b, 2),1/2)
     }
 
   }
 
+  //base colors
   const white = new Color(255, 255, 255);
   const silver = new Color(192, 192, 192);
   const gray = new Color(128, 128, 128);
@@ -53,35 +54,55 @@ function main(){
   const fuchsia = new Color(255, 0, 255);
   const purple = new Color(128, 0, 128);
 
-  const colorValues = [white, silver, gray, black, red, maroon,yellow, olive, lime, green, aqua, teal, blue, navy, fuchsia, purple];
-
+  //array of all colors, used to get absolute distance from each point
+  const colorValues = [white, silver, gray, black, red, maroon, yellow, olive, lime, green, aqua, teal, blue, navy, fuchsia, purple];
+  //used to get actual color names
   const colorNames = ["White", "Silver", "Gray", "Black", "Red", "Maroon", "Yellow", "Olive", "Lime", "Green", "Aqua", "Teal", "Blue", "Navy", "Fuchsia", "Purple"];
-  const numColors = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  
 
 
   function drawFrame(video) {
     ctx.drawImage(video, 0, 0);
     var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-    //console.log(imageData.data)
-    let currentPixel;
-    for(let i = 0; i < imageData.data; i += 4){
-      currentPixel = new Color(i, i+1, i+2);
-      let lowestIndex = 0;
-      for(let j = 0; j < colorValues.length(); j++){
+    //console.log(imageData.data);
+    const numColors = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-        let value = currentPixel.absoluteDist(colorValues[j])
-        if(value < colorValues[lowestIndex]){
+
+    for(let i = 0; i < 1499999; i += 4){
+      let currentPixel = new Color(i, i+1, i+2);
+
+      //console.log(i);
+      
+      //console.log(currentPixel.r)
+      for(let j = 0; j < colorValues.length; j++){
+
+        let value = currentPixel.distance(colorValues[j])
+        //console.log(value);
+        // if(value < currentPixel.distVals[lowestIndex]){
+        //   lowestIndex = j;
+        // }
+        currentPixel.distVals.push(value);
+        value = 0;
+      }
+      let lowestIndex = 15;
+
+      for(let j = 0; j < colorValues.length; j++){
+        if(currentPixel.distVals[j] < currentPixel.distVals[lowestIndex]){
           lowestIndex = j;
         }
-        currentPixel.distVals.push(value);
+
+        
       }
-      console.log(currentPixel.distVals)
+      //console.log(currentPixel.distVals)
+      //console.log(lowestIndex);
+      console.log(currentPixel.distVals);
       //currentPixel.color(colorNames[lowestIndex]);
       numColors[lowestIndex] = numColors[lowestIndex] + 1;
     }
 
-    console.log(numColors)
+
+    //console.log(numColors)
 
     setTimeout(function () {
       drawFrame(video);
